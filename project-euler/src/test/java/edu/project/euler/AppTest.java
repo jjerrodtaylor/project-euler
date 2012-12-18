aa4083b4243f84bbb89a734be2af392cc963ab83
jamaal.taylor <jamaal.taylor@six3systems.com>
2012-12-18 17:13:08 -0500

    Unit tests were added for problem 7.

--- project-euler/src/test/java/edu/project/euler/AppTest.java
+++ project-euler/src/test/java/edu/project/euler/AppTest.java
@@ -1,32 +1,40 @@
 package edu.project.euler;
 
 import junit.framework.*;
-import edu.project.euler.problem7;
-import edu.project.euler.problem8;
+import edu.project.euler.problem7.ProblemSeven;
+import edu.project.euler.problem8.ProblemEight;
+
+import java.util.ArrayList;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    //extends TestCase
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-
-    }
-
     /**
      * Rigourous Test :-)
      */
     public void testcalculateNthPrime()
     {
-        Problem
-        assertTrue( true );
+        ProblemSeven pSeven = new ProblemSeven();
+
+        //assertTrue(Actual,Expected)
+        assertEquals( 3, pSeven.calculateNthPrime(2));
+    }
+
+    public void testCheckForFactor()
+    {
+        ProblemSeven pSeven = new ProblemSeven();
+        assertEquals(true, pSeven.checkForFactor(4,2));
+    }
+
+    public void testCheckPreviousAnswer()
+    {
+        ProblemSeven pSeven = new ProblemSeven();
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
+        listOfNumbers.add(1);
+        listOfNumbers.add(2);
+        listOfNumbers.add(3);
+        assertEquals(true, pSeven.checkPreviousAnswers(2,listOfNumbers));
     }
 }
